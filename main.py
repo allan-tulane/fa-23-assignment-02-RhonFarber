1c7bfc7d3ae35f0d33b6becf59fcb4e38dac7a53
RhonFarber <122325331+RhonFarber@users.noreply.github.com>
2023-10-02 18:26:00 +0000

    Update 1.0

--- main.py
+++ main.py
@@ -4,59 +4,76 @@ See assignment-02.pdf for details.
 """
 import time
 
+
 class BinaryNumber:
-    """ done """
-    def __init__(self, n):
-        self.decimal_val = n               
-        self.binary_vec = list('{0:b}'.format(n)) 
-        
-    def __repr__(self):
-        return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
-    
+  """ done """
+
+  def __init__(self, n):
+    self.decimal_val = n
+    self.binary_vec = list('{0:b}'.format(n))
+
+  def __repr__(self):
+    return ('decimal=%d binary=%s' %
+            (self.decimal_val, ''.join(self.binary_vec)))
+
 
 ## Implement multiplication functions here. Note that you will have to
 ## ensure that x, y are appropriately sized binary vectors for a
 ## divide and conquer approach.
-def binary2int(binary_vec): 
-    if len(binary_vec) == 0:
-        return BinaryNumber(0)
-    return BinaryNumber(int(''.join(binary_vec), 2))
+def binary2int(binary_vec):
+  if len(binary_vec) == 0:
+    return BinaryNumber(0)
+  return BinaryNumber(int(''.join(binary_vec), 2))
+
 
 def split_number(vec):
-    return (binary2int(vec[:len(vec)//2]),
-            binary2int(vec[len(vec)//2:]))
+  return (binary2int(vec[:len(vec) // 2]), binary2int(vec[len(vec) // 2:]))
+
 
 def bit_shift(number, n):
-    # append n 0s to this number's binary string
-    return binary2int(number.binary_vec + ['0'] * n)
-    
-def pad(x,y):
-    # pad with leading 0 if x/y have different number of bits
-    # e.g., [1,0] vs [1]
-    if len(x) < len(y):
-        x = ['0'] * (len(y)-len(x)) + x
-    elif len(y) < len(x):
-        y = ['0'] * (len(x)-len(y)) + y
-    # pad with leading 0 if not even number of bits
-    if len(x) % 2 != 0:
-        x = ['0'] + x
-        y = ['0'] + y
-    return x,y
+  # append n 0s to this number's binary string
+  return binary2int(number.binary_vec + ['0'] * n)
 
+
+def pad(x, y):
+  # pad with leading 0 if x/y have different number of bits
+  # e.g., [1,0] vs [1]
+  if len(x) < len(y):
+    x = ['0'] * (len(y) - len(x)) + x
+  elif len(y) < len(x):
+    y = ['0'] * (len(x) - len(y)) + y
+  # pad with leading 0 if not even number of bits
+  if len(x) % 2 != 0:
+    x = ['0'] + x
+    y = ['0'] + y
+  return x, y
 
 
 def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+  x.binary_vec, y.binary_vec = pad(x.binary_vec, y.binary_vec)
 
+  if x.decimal_val <= 1 or y.decimal_val <= 1:
+    return BinaryNumber(x.decimal_val * y.decimal_val)
+
+  len_x = len(x.binary_vec)
+
+  xl, xr = split_number(x.binary_vec)
+  yl, yr = split_number(y.binary_vec)
+
+  xlyl = subquadratic_multiply(xl, yl)
+  xryr = subquadratic_multiply(xr, yr)
+
+  xl_plus_xr_yl_plus_yr = subquadratic_multiply(
+      BinaryNumber(xl.decimal_val + xr.decimal_val),
+      BinaryNumber(yl.decimal_val + yr.decimal_val))
+  middle_term = BinaryNumber(xl_plus_xr_yl_plus_yr.decimal_val -
+                             xlyl.decimal_val - xryr.decimal_val)
+  return BinaryNumber(
+      bit_shift(xlyl, len_x).decimal_val +
+      bit_shift(middle_term, len_x // 2).decimal_val + xryr.decimal_val)
 
 
 def time_multiply(x, y, f):
-    start = time.time()
-    # multiply two numbers x, y using function f
-    return (time.time() - start)*1000
-
-    
-    
-
+  start = time.time()
+  # multiply two numbers x, y using function f
+  return (time.time() - start) * 1000
